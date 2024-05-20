40324f6a1554d0fb94550b372e883d6a1c93be99
Luis Alburez <alburezluis@gmail.com>
2024-05-20 10:39:54 -0600

    subiendo Problema1

---
+++ Problema1.js
@@ -0,0 +1,7 @@
+let nombre = prompt("Please enter a name (try Brendan Eich)")
+
+if (nombre === "Brendan Eich") {
+    console.log("¡Correcto!")
+}else{
+    console.log("¿No lo sabes? ¡Brendan Eich!")
+}
