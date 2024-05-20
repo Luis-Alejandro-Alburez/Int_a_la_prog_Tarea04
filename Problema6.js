dde3acabbfd250c3380637b626f8072500f9527b
Luis Alburez <alburezluis@gmail.com>
2024-05-20 12:05:17 -0600

    agregando Problema6

---
+++ Problema6.js
@@ -0,0 +1,28 @@
+let cantidadNumeros = parseInt(prompt("Cuántos números desea ingresar?"));
+
+let numeros = [];
+let minNum = '';
+let maxNum = '';
+
+for (let i = 0; i < cantidadNumeros; i++) {
+    let numero = parseInt(prompt("Ingrese número: " + (i + 1)));
+    numeros.push(numero);
+}
+
+for (let j = 0; j < cantidadNumeros; j++) {
+    if (j === 0) {
+        minNum = numeros[j];
+        maxNum = numeros[j];        
+
+    } else {
+         if (numeros[j] < minNum) {
+             minNum = numeros[j];
+         } else if (numeros[j] > maxNum) {
+             maxNum = numeros[j];
+        }        
+    }
+}
+
+alert("los números son: " + numeros);
+alert("el mínimo es: " + minNum);
+alert("el máximo es: " + maxNum);
