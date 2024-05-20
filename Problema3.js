c1d7b18d59914edea457af8842fe2078476c69b2
Luis Alburez <alburezluis@gmail.com>
2024-05-20 11:08:32 -0600

    agregando Problema3

---
+++ Problema3.js
@@ -0,0 +1,7 @@
+let login = prompt('login:');
+let message = (login === 'Trabajador') ? 'Hola' :
+              (login === 'Dueño') ? 'Felicidades' :
+              (login === '') ? 'Sin sesión' : 
+              '';
+
+console.log(message);
